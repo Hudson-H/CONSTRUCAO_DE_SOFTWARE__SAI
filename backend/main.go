f07e800766cf5a093d06cac5e64726eeea33c567
Nícolas Carvalho <nicolas.mnw@gmail.com>
2024-11-29 18:10:36 -0300

    adicionado print ao executar o sistema

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sai-backend/app/route"
 	"sai-backend/app/shared/config"
@@ -8,6 +9,11 @@ import (
 )
 
 func main() {
+	fmt.Println(
+		"Inicializando servidor em: " +
+			"http://127.0.0.1:" + config.Configuration.SERVER_PORT,
+	)
+
 	db := database.CreateMySQLConnection(database.DBInfo{
 		User:     config.Configuration.DB_USER,
 		Password: config.Configuration.DB_PASSWORD,
