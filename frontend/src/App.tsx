8affc76a0e6466babb5ae3785ca2fa7c8fd7b195
Nícolas Carvalho <nicolas.mnw@gmail.com>
2025-01-15 17:11:23 -0300

    add Button; add Text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,7 @@ import { Routes } from './routes';
 import { _SampleProvider } from './context/_SampleContext';
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <_SampleProvider>
+    <Routes />
+  </_SampleProvider>
 }
-
-// export function App() {
-//   return <_SampleProvider>
-//         <Routes />
-//   </_SampleProvider>
-// }
