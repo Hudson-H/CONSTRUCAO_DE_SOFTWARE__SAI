4bf2a832435295f82013ea393f267f27118293a5
joaopl <jvplucas@hotmail.com>
2025-01-15 20:39:49 -0300

    feat/userRoute

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -24,4 +24,43 @@ const listarUsuarios = () => {
   });
 };
 
-module.exports = { adicionarUsuario, listarUsuarios };
+const listarUsuarioPorId = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM usuarios WHERE id = ?'; // Placeholder seguro para o parâmetro
+    db.query(query, [id], (err, results) => {
+      if (err) {
+        return reject('Erro ao buscar usuário: ' + err);
+      }
+      if (results.length === 0) {
+        return reject('Usuário não encontrado');
+      }
+      resolve(results[0]); // Retorna apenas o usuário encontrado
+    });
+  });
+};
+
+const deletarUsuario = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = 'DELETE FROM usuarios WHERE id = ?';
+    db.query(query, [id], (err, results) => {
+      if (err) {
+        return reject('Erro ao deletar usuário: ' + err);
+      }
+      resolve(results);
+    });
+  });
+}
+
+const atualizarUsuario = (id, nome, email) => {
+  return new Promise((resolve, reject) => {
+    const query = 'UPDATE usuarios SET nome = ?, email = ? WHERE id = ?';
+    db.query(query, [nome, email, id], (err, results) => {
+      if (err) {
+        return reject('Erro ao atualizar usuário: ' + err);
+      }
+      resolve(results);
+    });
+  });
+}
+
+module.exports = { adicionarUsuario, listarUsuarios, listarUsuarioPorId, deletarUsuario, atualizarUsuario };
