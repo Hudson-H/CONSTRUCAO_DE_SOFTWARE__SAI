0c8c930c52ace6f682d2292f22182979ffdf6728
João Pazinato <74716624+jeyeight@users.noreply.github.com>
2025-01-29 00:28:12 -0300

    Update userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,7 +6,7 @@ const autenticarUsuario = require('../middlewares/authMiddleware');
 
 router.patch('/usuarios/:id', autenticarUsuario, usuarioController.atualizarUsuario);
 router.get('/usuarios/:id', autenticarUsuario, usuarioController.listarUsuarioPorId);
-router.post('/usuarios', autenticarUsuario, usuarioController.adicionarUsuario);
+router.post('/usuarios', usuarioController.adicionarUsuario);
 router.get('/usuarios', autenticarUsuario, usuarioController.listarUsuarios);
 router.post('/usuarios/login', usuarioController.loginUsuario);
 router.delete('/usuarios/:id', autenticarUsuario, usuarioController.deletarUsuario);
